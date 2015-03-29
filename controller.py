e68c49092d4df10dd99c396a9381aca4d1fb9631
Kaladin Light <0.kaladin@gmail.com>
2015-03-28 20:58:02 -0400

    Code cleanup 3 by : https://github.com/roberttstephens/garage-door-controller

--- controller.py
+++ controller.py
@@ -16,10 +16,10 @@ from twisted.web.guard import HTTPAuthSessionWrapper, DigestCredentialFactory, B
 
 class HttpPasswordRealm(object):
     implements(portal.IRealm)
- 
+
     def __init__(self, myresource):
         self.myresource = myresource
-    
+
     def requestAvatar(self, user, mind, *interfaces):
         if IResource in interfaces:
             return (IResource, self.myresource, lambda: None)
@@ -40,9 +40,9 @@ class Door(object):
         self.time_to_close = config.get('time_to_close', 10)
         self.time_to_open = config.get('time_to_open', 10)
         gpio.setup(self.relay_pin, gpio.OUT)
-        gpio.setup(self.state_pin, gpio.IN, pull_up_down=gpio.PUD_UP)        
+        gpio.setup(self.state_pin, gpio.IN, pull_up_down=gpio.PUD_UP)
         gpio.output(self.relay_pin, True)
-        
+
     def get_state(self):
         if gpio.input(self.state_pin) == 0:
             return 'closed'
@@ -58,7 +58,7 @@ class Door(object):
                 return 'closing'
         else:
             return 'open'
-        
+
     def toggle_relay(self):
         state = self.get_state()
         if (state == 'open'):
@@ -70,7 +70,7 @@ class Door(object):
         else:
             self.last_action = None
             self.last_action_time = None
-        
+
         gpio.output(self.relay_pin, False)
         time.sleep(0.2)
         gpio.output(self.relay_pin, True)
@@ -88,13 +88,13 @@ class Controller(object):
         for door in self.doors:
             door.last_state = 'unknown'
             door.last_state_time = time.time()
-        
+
         self.use_smtp = False
-        smtp_params = ("smtphost", "smtpport", "smtp_tls", "username", 
+        smtp_params = ("smtphost", "smtpport", "smtp_tls", "username",
                        "password", "to_email", "time_to_wait")
         self.use_smtp = ('smtp' in config) and set(smtp_params) == set(config['smtp'])
-        syslog.syslog("Are we using SMTP: %s" % self.use_smtp)        
-            
+        syslog.syslog("Are we using SMTP: %s" % self.use_smtp)
+
     def status_check(self):
         open_doors = False
 
@@ -107,16 +107,16 @@ class Controller(object):
                 self.updateHandler.handle_updates()
             if not new_state == 'closed':
                 open_doors = True
-                
+
         if self.use_smtp:
             ttw = self.config['smtp']["time_to_wait"]
             if open_doors and not self.msg_sent and time.time() - self.open_time >= ttw:
                 self.send_opendoor_message(int(time.time() - self.open_time))
-        
+
         if not open_doors:
             self.open_time = time.time()
             self.msg_sent = False
-                
+
     def send_opendoor_message(self, opentime):
         syslog.syslog("Sending open door message. (%s)" % opentime)
         config = self.config['smtp']
@@ -127,8 +127,8 @@ class Controller(object):
         message = "Your garage doors have been open for %s." % elapsed_time(100+opentime)
         server.sendmail(config["username"], config["to_email"], message)
         server.close()
-        self.msg_sent = True    
-        
+        self.msg_sent = True
+
 
     def toggle(self, doorId):
         for d in self.doors:
@@ -136,7 +136,7 @@ class Controller(object):
                 syslog.syslog('%s: toggled' % d.name)
                 d.toggle_relay()
                 return
-        
+
     def get_updates(self, lastupdate):
         updates = []
         for d in self.doors:
@@ -149,7 +149,7 @@ class Controller(object):
         root = File('www')
         root.putChild('upd', self.updateHandler)
         root.putChild('cfg', ConfigHandler(self))
-        
+
         clk = ClickHandler(self)
         args={self.config['site']['username']:self.config['site']['password']}
         checker = checkers.InMemoryUsernamePasswordDatabaseDontUse(**args)
@@ -165,11 +165,11 @@ class Controller(object):
 
 class ClickHandler(Resource):
     isLeaf = True
-    
+
     def __init__ (self, controller):
         Resource.__init__(self)
         self.controller = controller
-    
+
     def render(self, request):
         door = request.args['id'][0]
         self.controller.toggle(door)
@@ -180,13 +180,13 @@ class ConfigHandler(Resource):
     def __init__ (self, controller):
         Resource.__init__(self)
         self.controller = controller
-    
+
     def render(self, request):
         request.setHeader('Content-Type', 'application/json')
-        
+
         return json.dumps([(d.id, d.name, d.last_state, d.last_state_time)
-                            for d in controller.doors])         
-        
+                            for d in controller.doors])
+
 
 class UpdateHandler(Resource):
     isLeaf = True
@@ -194,54 +194,54 @@ class UpdateHandler(Resource):
         Resource.__init__(self)
         self.delayed_requests = []
         self.controller = controller
-    
+
     def handle_updates(self):
         for request in self.delayed_requests:
             updates = self.controller.get_updates(request.lastupdate)
             if updates != []:
                 self.send_updates(request, updates)
                 self.delayed_requests.remove(request)
-    
+
     def format_updates(self, request, update):
         response = json.dumps({'timestamp': int(time.time()), 'update':update})
         if hasattr(request, 'jsonpcallback'):
             return request.jsonpcallback +'('+response+')'
         else:
             return response
-            
+
     def send_updates(self, request, updates):
         request.write(self.format_updates(request, updates))
         request.finish()
-    
+
     def render(self, request):
-        
+
         # set the request content type
         request.setHeader('Content-Type', 'application/json')
-        
+
         # set args
         args = request.args
-       
+
         # set jsonp callback handler name if it exists
         if 'callback' in args:
             request.jsonpcallback =  args['callback'][0]
-           
+
         # set lastupdate if it exists
         if 'lastupdate' in args:
             request.lastupdate = float(args['lastupdate'][0])
         else:
             request.lastupdate = 0
-            
-            #print "request received " + str(request.lastupdate)    
-            
+
+            #print "request received " + str(request.lastupdate)
+
         # Can we accommodate this request now?
         updates = controller.get_updates(request.lastupdate)
         if updates != []:
             return self.format_updates(request, updates)
-        
-        
+
+
         request.notifyFinish().addErrback(lambda x: self.delayed_requests.remove(request))
         self.delayed_requests.append(request)
-        
+
         # tell the client we're not done yet
         return server.NOT_DONE_YET
 
@@ -251,7 +251,7 @@ def elapsed_time(seconds, suffixes=['y','w','d','h','m','s'], add_s=False, separ
     """
     # the formatted time string to be returned
     time = []
-    
+
     # the pieces of time to iterate over (days, hours, minutes, etc)
     # - the first piece in each tuple is the suffix (d, h, w)
     # - the second piece is the length in seconds (a day is 60s * 60m * 24h)
@@ -261,7 +261,7 @@ def elapsed_time(seconds, suffixes=['y','w','d','h','m','s'], add_s=False, separ
              (suffixes[3], 60 * 60),
              (suffixes[4], 60),
              (suffixes[5], 1)]
-    
+
     # for each time piece, grab the value and remaining seconds, and add it to
     # the time string
     for suffix, length in parts:
@@ -274,12 +274,10 @@ def elapsed_time(seconds, suffixes=['y','w','d','h','m','s'], add_s=False, separ
             break
 
     return separator.join(time)
-     
+
 if __name__ == '__main__':
     syslog.openlog('garage_controller')
     config_file = open('config.json')
     controller = Controller(json.load(config_file))
     config_file.close()
     controller.run()
-              
-    
